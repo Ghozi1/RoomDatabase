946fdec059aca8accb03f9f4a97f0a2713031485
Ghozi <mghozighifar04@gmail.com>
2023-12-06 04:23:28 +0700

    Menghentikan perilaku aliran data selama 5000 mili second/ sekitar 5 second

--- app/src/main/java/com/example/roomdatabase/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/HomeViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
-
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
