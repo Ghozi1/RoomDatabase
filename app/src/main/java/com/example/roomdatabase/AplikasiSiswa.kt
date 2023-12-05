1a5cdcd703f7466a4f6616c722038d3666163187
Ghozi <mghozighifar04@gmail.com>
2023-12-06 04:30:03 +0700

    Menhubungkan file dengan depedensi dari kelas yang berada di file lain

--- app/src/main/java/com/example/roomdatabase/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/AplikasiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.roomdatabase
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomdatabase.repositori.ContainerApp
+import com.example.roomdatabase.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application(){
+    lateinit var container : ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
