0f3104b9e3e784cb5b9ca86e16641f94c2a2f8e8
Ghozi <mghozighifar04@gmail.com>
2023-12-06 03:55:01 +0700

    First

---
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -0,0 +1,68 @@
+package com.example.roomdatabase.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.data.Siswa
+import com.example.roomdatabase.repositori.OfflineRepositoriSiswa
+
+class EntryViewModel (private val repositoriSiswa: OfflineRepositoriSiswa): ViewModel() {
+
+    /*Berisi Status Siswa Saat Ini*/
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+    private set
+
+    /*
+    * Fungsi Untuk memvalidasi input
+    */
+    private fun validasiInput(uiState: DetailSiswa= uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry*/
+    suspend fun saveSiswa(){
+        if(validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+/*Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya*/
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStatesiswa(isEntryValid: Boolean =false): UIStateSiswa= UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama ,
+    alamat = alamat,
+    telpon = telpon
+)
+
