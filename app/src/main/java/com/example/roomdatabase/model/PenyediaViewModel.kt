27c0b825defe5a45b29a9e23f3d502b4e4914dff
Ghozi <mghozighifar04@gmail.com>
2023-12-06 04:32:58 +0700

    Menambahkan function pada objeck PenyediaViewModel

--- app/src/main/java/com/example/roomdatabase/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/PenyediaViewModel.kt
@@ -1,5 +1,12 @@
 package com.example.roomdatabase.model
 
 object PenyediaViewModel {
-
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
 }
