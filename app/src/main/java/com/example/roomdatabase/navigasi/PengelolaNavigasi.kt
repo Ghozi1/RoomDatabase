239d1307a518148621a314bc3a0b54e84cee0e9e
Ghozi <mghozighifar04@gmail.com>
2023-12-06 04:50:26 +0700

    Fungsi komposisi untuk aplikasi Siswa menggunakan NavController di Jetpack Compose.

--- app/src/main/java/com/example/roomdatabase/navigasi/PengelolaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/navigasi/PengelolaNavigasi.kt
@@ -1,2 +1,10 @@
 package com.example.roomdatabase.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+
+}
