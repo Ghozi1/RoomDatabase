a8039b9c123beeb394e742435e877022f78d0a0e
Ghozi <mghozighifar04@gmail.com>
2023-12-06 04:45:01 +0700

    Navigasi tujuan dengan rute dan judul untuk aplikasi Android.

--- app/src/main/java/com/example/roomdatabase/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomdatabase/ui/halaman/HalamanEntry.kt
@@ -1,4 +1,9 @@
 package com.example.roomdatabase.ui.halaman
 
-object HalamanEntry {
+import com.example.roomdatabase.R
+import com.example.roomdatabase.navigasi.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
